26808d8a055e1cbbff10b995f45b51f0fa2181d8
Saif-El-Eslam <76692693+Saif-El-Eslam@users.noreply.github.com>
2022-06-23 22:42:45 +0200

    Project's files

---
+++ Enemies/Active_Freezers.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "Enemy.h"
+#include "..\Castle\Castle.h"
+
+//Active Enemy: is an enemy with Arrival Time ≤ current time step & Health > 0
+//Freezer can throw ice on the castle to frost it for some time.
+class Active_Freezers : public Enemy
+{
+private:
+	int CurrentHealth;
+//	int CurrentSpeed; //they are protected in enemy.h
+	//int freezetime;
+	//int Distance;
+public:
+	Active_Freezers(int id, int arrTime, int d = MaxDistance);
+	~Active_Freezers();
+
+	void DamageToEnemy(int castlefirepower);
+	bool IsitTimeToShot();
+	//int TimeToShot();
+
+	void Move();	//All enemies can move
+	void Act(Castle* C);	//Acting means fighting or healing
+
+	//void SetCurrentHealth(int CH);
+	//int GetCurrentHealth() const;
+	void SetCurrentHealth(double CH);
+	double GetCurrentHealth();
+	//void SetDistance(int);
+	//int GetDistance() const;
+	/*void SetCurrentSpeed(int CS);
+	int GetCurrentSpeed() const;*/
+
+	//void SingleShot();//logic for act function
+};
